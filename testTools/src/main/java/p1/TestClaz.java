78b5fe88c0777dd83f9abc2f5e2c99ffd516b6b9
uahappy <765208390@qq.com>
2021-02-19 02:02:33 +0800

    添加代码

--- testTools/src/main/java/p1/TestClaz.java
+++ testTools/src/main/java/p1/TestClaz.java
@@ -3,11 +3,11 @@ package p1;
 public class TestClaz {
     public static void main(String[] args) {
 
-
         int a = 1;
         int b = 2;
+
         int c = 3;
 
-        System.out.println("the first protest");
+        System.out.println("the first protest"+c);
     }
 }
